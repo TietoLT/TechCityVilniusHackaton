87fe2108be516cce159e14260984b61a820de845
Mantas <marciman@WL2012469.eu.tieto.com>
2014-06-28 11:54:34 +0300

    Database model

--- GoogleMapsWEB/Controllers/HomeController.cs
+++ GoogleMapsWEB/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DatabaseLibrary;
+using System.Data.Entity;
 namespace SportiskasVilnius.Controllers
 {
     public class HomeController : Controller
@@ -11,7 +13,11 @@ namespace SportiskasVilnius.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
+            using (var ctx = new SContext())
+            {
+                ctx.Events.Add(new Event() {Date = DateTime.Now, Desc = "", Id = 1, IsActive = true, LocX = "1", LocY = "1", Owner = "ME"});
+                ctx.SaveChanges();
+            }
             return View();
         }
 
