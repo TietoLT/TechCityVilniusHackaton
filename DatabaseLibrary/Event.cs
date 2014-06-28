084d88487033d2c04772b4e64a14c50a7cd4ddce
Mantas <marciman@WL2012469.eu.tieto.com>
2014-06-28 11:33:08 +0300

    Event Model fill

--- DatabaseLibrary/Event.cs
+++ DatabaseLibrary/Event.cs
@@ -8,6 +8,12 @@ namespace DatabaseLibrary
 {
     public class Event
     {
-       
+        public String Owner { get; set; }
+        public Int32 Id { get; set; }
+        public String Desc { get; set; }
+        public String LocX { get; set; }
+        public String LocY { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean IsActive { get; set; }
     }
 }
